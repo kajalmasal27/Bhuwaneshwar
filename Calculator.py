4a09dd0c8165d32b37e507a0e569e9a9519d5047
kajalmasal27 <kajalmasal27@gmail.com>
2022-11-03 20:14:52 +0530

    Code for addition and subtraction.

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+def addition(a,b):
+    add =  a+b
+    print(f"Addition of {a} and {b} is {add}.")
+
+def subtraction(a,b):
+    sub = a-b
+    print(f"Addition of {a} and {b} is {sub}.")
+
+
+addition(3,5)
+subtraction(5,9)
