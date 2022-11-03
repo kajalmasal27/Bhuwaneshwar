063a42323b73a300fac1891b79cd0a74d73429b4
kajalmasal27 <kajalmasal27@gmail.com>
2022-11-03 23:25:50 +0530

    amz-001 added code for multiplication.

--- Calculator.py
+++ Calculator.py
@@ -6,6 +6,10 @@ def subtraction(a,b):
     sub = a-b
     print(f"Addition of {a} and {b} is {sub}.")
 
+def multiplication(a,b):
+    mul = a*b
+    print(f"Multiplication  of {a} and {b} is {mul}.")
 
 addition(3,5)
 subtraction(5,9)
+multiplication(9,8)
